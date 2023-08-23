92c9b7a77fb354f5582fbded27d591706ea16aa8
jpomfret <jpomfret7@gmail.com>
2023-08-23 16:56:37 +0000

    new func

---
+++ demos/AzureFunction/functions/InsertSqlData/run.ps1
@@ -0,0 +1,19 @@
+using namespace System.Net
+
+param($Request)
+
+Write-Host "PowerShell function with SQL Output Binding processed a request."
+
+# Update req_body with the body of the request
+$req_body = $Request.Body
+
+write-host $req_body.tostring()
+
+# Assign the value we want to pass to the SQL Output binding. 
+# The -Name value corresponds to the name property in the function.json for the binding
+Push-OutputBinding -Name todoItems -Value $req_body
+
+Push-OutputBinding -Name res -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $req_body
+})
