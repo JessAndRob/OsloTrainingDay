9d78c451ad22d19b6905bb831274484dbac5868f
SQLDbaWithABeard <mrrobsewell@outlook.com>
2023-08-27 12:38:03 +0100

    Cos state is so great and ate steak

--- demos/IaC/terraform/backend.tf
+++ demos/IaC/terraform/backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "dbatools-azure-lab"
+    storage_account_name = "testpostman1233456"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
